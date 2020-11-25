666fb553be7d9a0670829f71e6658452e1c8e222
unknown <skasraf001@gmail.com>
2020-11-25 18:48:37 +0530

    added the images

--- js/Game.js
+++ js/Game.js
@@ -30,25 +30,36 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1Img);
+
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2Img);
+
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3Img);
+
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4Img);
+
     cars = [car1, car2, car3, car4];
   }
 
   play(){
+
     form.hide();
 
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+     background(ground); 
+
+     image(track,0,-displayHeight*4,displayWidth,displayHeight*5)
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 150;
       var y;
 
       for(var plr in allPlayers){
@@ -78,7 +89,13 @@ class Game {
       player.distance +=10
       player.update();
     }
+    if(player.distance>3860){
+      gameState=2;
+    }
 
     drawSprites();
+  }
+  end(){
+    console.log("Game Ended");
   }
 }
